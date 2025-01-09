a9375a85cbef25b4078603d43484277991432ba1
Zasadar <zasada.r89@gmail.com>
2025-01-10 02:09:33 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Max";
+            Console.WriteLine(myName);
+            Console.ReadLine();
+        }
+    }
+}
