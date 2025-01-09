84918c82edca3dccedc583b7448e8aa60a86bd6f
Zasadar <zasada.r89@gmail.com>
2025-01-10 03:13:26 +0500

    Добавлены табуляции, и перенос строки, логические литералы

--- Program.cs
+++ Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
-            const string myName = "Максим";
-            Console.WriteLine(myName);
+            const string MyName = "Максим";
+            Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Hello World!");
+            Console.WriteLine("\t Мне 35 лет");
+            Console.WriteLine("\t My name is \n Max");
+
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
             Console.ReadKey();
         }
     }
