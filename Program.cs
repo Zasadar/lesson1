2b0a418aa447f5c7ba757794e6731346051ec957
Zasadar <zasada.r89@gmail.com>
2025-01-19 15:21:54 +0500

    Цикл For - начало...

--- Program.cs
+++ Program.cs
@@ -10,26 +10,38 @@ namespace lesson1
         {
             Console.WriteLine("Напишите совй любимый цвет на английском с маленькой буквы");
 
-            var color = Console.ReadLine();
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Итерация {0}", i);
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш цвет красный!");
+                        break;
 
-            if (color == "red")
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Ваш цвет красный!");
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш цвет зеленый!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш цвет бирюзовый!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ваш цвет желтый!");
+                        break;
+                }
             }
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Ваш цвет зеленый!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Ваш цвет бирюзовый!");
-            }
+
+
         }
     }
 }
